8c3a1d4fdca36826f2fa0959306b353b5a71f25a
adilrza0 <adilrza0@gmail.com>
2024-01-19 01:28:06 +0530

    Navbar done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './Component/Navbar';
+import AllRoutes from './Routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <AllRoutes/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='bg-blue-500'>Home</h1>
+    </div>
+  )
+}
